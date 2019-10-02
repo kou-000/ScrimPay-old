502e7255c727f25bb469329b0ee48ae0b14348b1
kou-000 <p231067k@mail.kyutech.jp>
2019-10-02 15:52:06 +0900

    Create DataBase.

--- KLab/ScrimPay/admin.py
+++ KLab/ScrimPay/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Main, User, Service
+
+admin.site.register(Main)
+admin.site.register(User)
+admin.site.register(Service)
