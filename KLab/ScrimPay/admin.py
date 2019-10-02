b5717bb636891c5c54bb2f9410e3a71e65f310d5
kou-000 <p231067k@mail.kyutech.jp>
2019-10-02 16:43:06 +0900

    Return to first commit.

--- KLab/ScrimPay/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-
-# Register your models here.
-from .models import Main, User, Service
-
-admin.site.register(Main)
-admin.site.register(User)
-admin.site.register(Service)
